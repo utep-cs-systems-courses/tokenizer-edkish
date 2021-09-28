03c7555b938105ba11232ea5dffd4e96b23010bc
edkish <edkish@miners.utep.edu>
2021-09-27 22:52:27 -0600

    Fixed minor errors

---
+++ src/.#uimain.c
@@ -0,0 +1,1 @@
+student@systems-vm.1921:1632800688
---
+++ src/uimain.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tokenizer.h"
+#include "history.h"
+
+#define MAX 1000
+
+
+int main()
+{
+  List* list = init_history();
+
+
+  char input[MAX];
+  int id;
+  while (1) {
+    printf("\nType a command: 0,t,h,s \n $ ");
+    fgets(input, MAX, stdin);
+    input[1] = '\0';
+    if (strlen(input)==1 && input[0]=='0') {
+      printf("Now exiting program \n");
+      break;
+    }
+
+
+    if (strlen(input)==1 && input[0]=='t') { //t
+      printf("Enter String to tokenize: \n");
+      printf("$ ");
+      fgets(input, MAX, stdin);
+      char **tk = tokenize(input);
+      add_history(list, input);
+      print_tokens(tk);
+      free_tokens(tk);
+    }
+
+
+    if (strlen(input)==1 && input[0]=='h') { //h
+      print_history(list);
+    }
+
+
+    if (strlen(input)==1 && input[0]=='s') { //s
+      printf("Enter sequence number: \n");
+      printf("$ ");
+      scanf("%d", &id);
+      fflush(stdin);
+      char* n = get_history(list, id);
+      printf(n);
+      printf("\n");
+    }
+  }
+
+
+  free_history(list);
+
+}
